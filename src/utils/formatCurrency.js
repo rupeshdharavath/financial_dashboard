9fe53c7fe5c122ddba8ef32389cba2665d7efbba
Umesh Dharavath <rupeshdharavath@gmail.com>
2026-04-05 23:45:05 +0530

    completed all

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -1,7 +1,7 @@
 export function formatCurrency(value) {
-	return new Intl.NumberFormat('en-US', {
+	return new Intl.NumberFormat('en-IN', {
 		style: 'currency',
-		currency: 'USD',
+		currency: 'INR',
 		maximumFractionDigits: 0,
 	}).format(value)
 }
